866caa89a3931ece62c8ae0e3ef5a811ad4880df
blerude <benjamin.lerude@yale.edu>
2017-08-02 13:15:28 -0700

    it's broken'

--- server/slack.js
+++ server/slack.js
@@ -7,7 +7,6 @@ var User = require('../models.js').User;
 var Reminder = require('../models.js').Reminder;
 var Meeting = require('../models.js').Meeting;
 
-
 /*
  * Example for creating and working with the Slack RTM API.
  */
@@ -20,15 +19,8 @@ let channel;
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for (const c of rtmStartData.channels) {
-      if (c.name === 'general') { channel = c.id }
+    if (c.name === 'general') { channel = c.id }
   }
-
-  User.find({}, function(err, users) {
-    users.forEach(user => {
-      user.tokens = {};
-      user.save();
-    })
-  })
 
   var today = new Date().getTime();
   var tomorrow = today + (1000 * 60 * 60 * 24)
@@ -41,7 +33,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
       }
     })
   })
-  ////
+
   var users = rtmStartData.users;
   users.forEach(user => {
     User.findOne({slackId: user.id}, function(err, foundUser) {
@@ -53,7 +45,6 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
           slackId: user.id,
           slackUsername: user.name,
           pending: '',
-          // channel:
         }).save(function(err, savedUser) {
           if (err) {
             res.send(err)
@@ -64,41 +55,15 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
       }
     })
   })
+
+  User.find({}, function(err, users) {
+    users.forEach(user => {
+      user.tokens = {};
+      user.save();
+    })
+  })
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 })
-
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('MESSAGE', message);
-  axios({
-    method: 'post',
-    url: 'https://api.api.ai/v1/query?v=20150910',
-    headers: {
-      "Authorization": "Bearer a53d802617124f92b9a6d63c76dd2d08",
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    data: {
-      query: message.text,
-      // context: [{
-      //     name: "weather",
-      //     lifespan: 4
-      // }],
-      // location: {
-      //     latitude: 37.459157,
-      //     longitude: -122.17926
-      // },
-      // timezone: "America/New_York",
-      lang: "en",
-      sessionId: message.user
-    }
-  })
-  .then(function (response) {
-    console.log('DATA', response);
-    rtm.sendMessage(response.data.result.fulfillment.speech, message.channel);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-});
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('USER', message.user);
@@ -114,8 +79,6 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
           //   console.log('Message denied.');
           // }
         })
-      // } else if (!sentUser.googleCalendarAccount) {
-      //   console.log("authorization")
       } else {
         axios({
           method: 'post',
@@ -126,15 +89,6 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
           },
           data: {
             query: message.text,
-            // context: [{
-            //     name: "weather",
-            //     lifespan: 4
-            // }],
-            // location: {
-            //     latitude: 37.459157,
-            //     longitude: -122.17926
-            // },
-            // timezone: "America/New_York",
             lang: "en",
             sessionId: message.user
           }
@@ -206,7 +160,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
                     invitee: response.data.result.parameters.invitee,
                     time: response.data.result.parameters.time
                   });
-                  // foundUser.channel = message.channel
+                  foundUser.channel = message.channel
                 }
                 foundUser.save()
                 .then(resp2 => {
@@ -279,12 +233,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     })
   }
 });
-// rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
-//   console.log('Reaction added:', reaction);
-// });
-// rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
-//   console.log('Reaction removed:', reaction);
-// });
+
 module.exports = {
   rtm,
   web
